fad345f6eea43844e8721927aed34f99844d6129
WeszeRita <weszelovszkirita@gmail.com>
2024-04-11 15:40:00 +0200

    fixes

--- src/app/componenets/template-outlet/template-outlet.component.ts
+++ src/app/componenets/template-outlet/template-outlet.component.ts
@@ -5,8 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './template-outlet.component.html',
 })
 export class TemplateOutletComponent {
-  context = {
-    $implicit: 'World',
-    localHu: 'Jani',
-  };
 }
