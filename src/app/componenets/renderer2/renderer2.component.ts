ce79edc963de1f54bee15c43c6cc33c0c1c6ee34
WeszeRita <weszelovszkirita@gmail.com>
2024-04-11 15:41:29 +0200

    example for renderer2

---
+++ src/app/componenets/renderer2/renderer2.component.ts
@@ -0,0 +1,26 @@
+import { Component, ElementRef, OnInit, Renderer2 } from '@angular/core';
+
+@Component({
+  selector: 'app-renderer2',
+  templateUrl: './renderer2.component.html',
+})
+export class Renderer2Component implements OnInit {
+  painters = ['Leonardo da Vinci', 'Vincent van Gogh', 'Pablo Picasso', 'Michelangelo', 'Rembrandt', 'Claude Monet'];
+
+  constructor(private renderer: Renderer2, private elementRef: ElementRef) {}
+
+  ngOnInit(): void {
+    const ul = this.renderer.createElement('ul');
+    this.renderer.appendChild(this.elementRef.nativeElement, ul);
+
+    this.painters.forEach((painter, index)=> {
+      const li = this.renderer.createElement('li');
+      const liText = this.renderer.createText(painter);
+      this.renderer.appendChild(li, liText);
+      index % 2 === 0
+        ? this.renderer.setStyle(li, 'color', '#F08080')
+        : this.renderer.setStyle(li, 'color', '#48D1CC')
+      this.renderer.appendChild(ul, li);
+    })
+  }
+}
